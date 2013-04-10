2726487716eb32920df09b086fca077f79ddac70
RyanDurling <ryan.p.durling@gmail.com>
2013-04-09 20:28:30 -0700

    Defining a Method on the Scope

--- SharingDataBetweenControllers/main.js
+++ SharingDataBetweenControllers/main.js
@@ -1,3 +1,11 @@
-function FirstCtrl() {}
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function() {
+    return {message: "I'm data from a service"};
+});
+function FirstCtrl($scope, Data) {
+    $scope.data = Data;
+}
 
-function SecondCtrl() {}
+function SecondCtrl($scope, Data) {
+    $scope.data = Data;
+}
